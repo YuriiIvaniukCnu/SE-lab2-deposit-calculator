d409b4d074b227193c4f5db388fb7a337c4fb7ee
yuriy <yuriy.iwanyuk@gmail.com>
2026-09-14 09:49:08 +0300

    Add calculator logic

--- lab2.py
+++ lab2.py
@@ -1,7 +1,14 @@
 def main():
     amount = float(input("Enter deposit amount: "))
-    interest = int(input("Enter interest rate: "))
+    interest_year = int(input("Enter interest rate (in %): "))
     term = int(input("Enter deposit term (in months): "))
+
+    interest_month = (interest_year / 100) / 12
+    for i in range(0, term):
+        amount += amount * interest_month
+        print(amount, " ", i, " ", interest_month)
+
+    print("Balance:", amount)
 
 if __name__ == "__main__":
     main()
