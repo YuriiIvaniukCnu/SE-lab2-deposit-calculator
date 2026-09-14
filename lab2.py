3731b5b7b0a942f7b13ff39346f3f880b29622c3
yuriy <yuriy.iwanyuk@gmail.com>
2026-09-14 09:32:47 +0300

    Program setup

---
+++ lab2.py
@@ -0,0 +1,7 @@
+def main():
+    amount = float(input("Enter deposit amount: "))
+    interest = int(input("Enter interest rate: "))
+    term = int(input("Enter deposit term (in months): "))
+
+if __name__ == "__main__":
+    main()
