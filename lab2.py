590dbd71060a88c29c5bcf57b4422ded17de9289
yuriy <yuriy.iwanyuk@gmail.com>
2026-09-14 10:06:05 +0300

    Restructure code and add minor improvement

--- lab2.py
+++ lab2.py
@@ -1,14 +1,18 @@
-def main():
-    amount = float(input("Enter deposit amount: "))
-    interest_year = int(input("Enter interest rate (in %): "))
-    term = int(input("Enter deposit term (in months): "))
+def calculate_deposit(amount, interest_year, term):
+    print(" ")
 
     interest_month = (interest_year / 100) / 12
     for i in range(0, term):
         amount += amount * interest_month
-        print(amount, " ", i, " ", interest_month)
+        print("Month: ",i+1, " | ", amount)
 
-    print("Balance:", amount)
+    print("\nTotal:", amount)
+def main():
+    amount = float(input("Enter deposit amount: "))
+    interest_year = int(input("Enter interest rate (% annual): "))
+    term = int(input("Enter deposit term (months): "))
+
+    calculate_deposit(amount, interest_year, term)
 
 if __name__ == "__main__":
     main()
